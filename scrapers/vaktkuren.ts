109218ba9d379c5cdb057d46382e25b0752999fb
Svante Bengtson <svante@swantzter.se>
2024-03-18 13:25:02 +0100

    add vaktkuren

---
+++ scrapers/vaktkuren.ts
@@ -0,0 +1,30 @@
+import * as cheerio from 'cheerio'
+import { DataError, FetchError, getUserAgent, saveHtml, type RestaurantInfo, type MenuFile } from '../helpers'
+
+export const info: RestaurantInfo = { id: 'vaktkuren', name: 'Vaktkurens Mos', autoplayDuration: 10_000 }
+
+export default async function scrape (): Promise<MenuFile[]> {
+  const baseUrl = 'http://www.vaktkuren.se/meny.asp'
+  const res = await fetch(baseUrl, {
+    headers: {
+      accept: 'text/html',
+      'cache-control': 'no-cache',
+      'user-agent': getUserAgent()
+    }
+  })
+  if (!res.ok) throw new FetchError(res.url, res.status, await res.text())
+
+  const body = await res.arrayBuffer()
+  const bodyText = new TextDecoder('windows-1252').decode(body)
+  const $ = cheerio.load(bodyText)
+
+  const menu = $('#print_content').html()
+  if (menu == null) throw new DataError('Could not find menu', res.url, body)
+
+  await saveHtml(menu, 'vaktkuren.html')
+
+  return [{
+    type: 'html',
+    src: '/assets/vaktkuren.html'
+  }]
+}
