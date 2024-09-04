cd997628659aca6442a23094966c93feed1fb7fb
Svante Bengtson <svante@swantzter.se>
2024-09-04 11:33:12 +0200

    remove eld och vin storgatan

--- scrapers/eldovin-heimdallgatan.ts
+++ scrapers/eldovin-heimdallgatan.ts
@@ -1,6 +1,27 @@
-import { type RestaurantInfo } from '../helpers'
-import { scrapeFactory } from './eldovin-storgatan'
+import * as cheerio from 'cheerio'
+import { DataError, FetchError, type RestaurantInfo, getUserAgent, saveHtml } from '../helpers'
+import { fetch } from 'undici'
 
 export const info: RestaurantInfo = { id: 'eldovin-heimdallgatan', name: 'Eld&Vin Heimdallgatan', autoplayDuration: 10_000 }
 
-export default scrapeFactory('heimdallgatan')
+export default async function scrape () {
+  const baseUrl = new URL('https://eldochvin.se/lunch/')
+  const res = await fetch(baseUrl, {
+    headers: {
+      accept: 'text/html',
+      'cache-control': 'no-cache',
+      'user-agent': getUserAgent(),
+    },
+  })
+  if (!res.ok) throw new FetchError(res.url, res.status, await res.text())
+
+  const body = await res.text()
+  const $ = cheerio.load(body)
+
+  $('#lunchmeny a').remove()
+  const menu = $('#lunchmeny').html()
+  if (menu == null) throw new DataError('Could not find menu', res.url, body)
+
+  await saveHtml(menu, 'eldovin-heimdallgatan.html')
+  return [{ type: 'html', src: '/assets/eldovin-heimdallgatan.html' }]
+}
