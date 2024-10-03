7030f399132fab3d5a084a577936b79121a421c9
Svante Bengtson <svante@swantzter.se>
2024-10-03 10:51:39 +0200

    add Pavo

---
+++ scrapers/pavo.ts
@@ -0,0 +1,27 @@
+import * as cheerio from 'cheerio'
+import { DataError, FetchError, type RestaurantInfo, getUserAgent, saveHtml, type MenuFile } from '../helpers'
+import { fetch } from 'undici'
+
+export const info: RestaurantInfo = { id: 'pavo', name: 'Pavo', autoplayDuration: 10_000 }
+
+export default async function scrape (): Promise<MenuFile[]> {
+  const baseUrl = new URL('https://restaurangpavo.se/')
+  const res = await fetch(baseUrl, {
+    headers: {
+      accept: 'text/html',
+      'cache-control': 'no-cache',
+      'user-agent': getUserAgent(),
+    },
+  })
+  if (!res.ok) throw new FetchError(res.url, res.status, await res.text())
+
+  const body = await res.text()
+  const $ = cheerio.load(body)
+
+  const menu = $('#lunchmeny').parent().html()
+  if (menu == null) throw new DataError('Could not find menu element', res.url, body)
+
+  await saveHtml(menu, 'pavo.html')
+
+  return [{ type: 'html', src: '/assets/pavo.html' }]
+}
