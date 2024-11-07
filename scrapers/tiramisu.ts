88779957475850004f46a7f1a673ac2927f1f2f0
Svante Bengtson <svante@swantzter.se>
2024-11-07 16:19:06 +0100

    scrape tiramisu

--- scrapers/tiramisu.ts
+++ scrapers/tiramisu.ts
@@ -1,3 +1,27 @@
-import { type RestaurantInfo } from '../helpers'
+import { DataError, FetchError, getUserAgent, MenuFile, saveHtml, type RestaurantInfo } from '../helpers'
+import * as cheerio from 'cheerio'
+import { fetch } from 'undici'
 
-export const info: RestaurantInfo = { id: 'tiramisu', name: 'Tiramisu' }
+export const info: RestaurantInfo = { id: 'tiramisu', name: 'Tiramisu', autoplayDuration: 10_000 }
+
+export default async function scrape (): Promise<MenuFile[]> {
+  const baseUrl = new URL('https://tiramisuu.se/')
+  const res = await fetch(baseUrl, {
+    headers: {
+      accept: 'text/html',
+      'cache-control': 'no-cache',
+      'user-agent': getUserAgent(),
+    },
+  })
+  if (!res.ok) throw new FetchError(res.url, res.status, await res.text())
+
+  const body = await res.text()
+  const $ = cheerio.load(body)
+
+  const menu = $('.et_pb_section_1').html()
+  if (menu == null) throw new DataError('Could not find menu element', res.url, body)
+
+  await saveHtml(menu, 'tiramisu.html')
+
+  return [{ type: 'html', src: '/assets/tiramisu.html' }]
+}
